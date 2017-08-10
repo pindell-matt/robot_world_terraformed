b3ff7b6d5b2723c7e4385704cce2bb66df08c74a
pindell-matt <pindell.m@protonmail.com>
2017-08-10 16:45:40 -0700

    add new app/ dir containing Elm frontend

---
+++ app/elm/Main.elm
@@ -0,0 +1,6 @@
+module Main exposing (..)
+
+import Html exposing (text)
+
+main =
+  text "Terraformed Robot World! Rocket.rs + Elm"
---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+import Elm from './main';
+
+const elmDiv = document.querySelector('#elm-container');
+
+if (elmDiv) {
+  Elm.Main.embed(elmDiv);
+}
